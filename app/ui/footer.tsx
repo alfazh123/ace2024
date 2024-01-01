24475d942d13520ee8fba7b9a29f48580092ed50
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-01 22:27:35 +0700

    fix: add some link and change address

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="bg-slate-200 text-slate-900 pt-10">
-      <div className="container px-20 py-4 flex justify-between items-center">
+      <div className="container md:px-20 px-10 py-4 flex flex-col md:flex-row md:justify-between md:items-center space-y-5 md:space-x-5">
         <figure className="w-20">
           <Image
             src="/next.svg"
@@ -14,18 +16,21 @@ export default function Footer() {
           />
         </figure>
         <div>
-          <p className="text-xl font-bold">Kontak</p>
-          <p>Phone: 0812-3456-7890</p>
+          <p className="text-xl font-bold">Sosial</p>
+          <Link href="" className="flex">
+            <FaInstagram className={`mr-2 text-xl`} />
+            ACE2024
+          </Link>
         </div>
         <div>
           <p className="text-xl font-bold">Alamat</p>
           <p>
-            Jl. Raya ITS, Keputih, Kec. Sukolilo, Kota SBY, Jawa Timur 60111
+            Jl. Raya Bungah Nomor 1 Sampurnan Bungah 61152, Gresik, Jawa Timur
           </p>
         </div>
       </div>
       <div className="flex justify-between mx-auto px-20 py-10">
-        <p className="text-sm">© 2021 ACE</p>
+        <p className="text-sm">© 2023 ACE</p>
         <p className="text-sm">All rights reserved</p>
       </div>
     </footer>
