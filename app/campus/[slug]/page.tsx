2849bd8f5c5a5942dfbf0be8ca71baa3e6ba5431
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-06 14:47:26 +0700

    make slug for campus blog

--- app/campus/[slug]/page.tsx
+++ app/campus/[slug]/page.tsx
@@ -1,8 +1,44 @@
-import Markdown from "markdown-to-jsx";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const PostPage = (props: any) => {
-  const slug = props.params.slug;
-  return <div>{slug}</div>;
-};
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-export default PostPage;
+export async function generateStaticParams() {
+  const files = fs.readdirSync(path.join("campus"));
+
+  const paths = files.map((filename) => ({
+    slug: filename.replace(".mdx", ""),
+  }));
+
+  return paths;
+}
+
+function getPost({ slug }: { slug: string }) {
+  const markdownFile = fs.readFileSync(
+    path.join("campus", slug + ".mdx"),
+    "utf-8"
+  );
+
+  const { data: frontMatter, content } = matter(markdownFile);
+
+  return {
+    frontMatter,
+    slug,
+    content,
+  };
+}
+
+export default function Campus({ params }: any) {
+  const props = getPost(params);
+
+  return (
+    <article className="min-h-screen flex flex-col prose prose-sm md:prose-base lg:prose-lg !prose-slate progress-invert m-auto">
+      <h1 className="md:mt-32 mt-32">{props.frontMatter.title}</h1>
+
+      <div className="text-justify mb-32">
+        <MDXRemote source={props.content}></MDXRemote>
+      </div>
+    </article>
+  );
+}
