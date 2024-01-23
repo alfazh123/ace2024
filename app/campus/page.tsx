070788084ec1372f4b98d177202543e46bc07ad3
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-23 20:33:26 +0700

    feat: use search params for filtering campus

--- app/campus/page.tsx
+++ app/campus/page.tsx
@@ -3,8 +3,16 @@ import matter from "gray-matter";
 import Link from "next/link";
 import path from "path";
 import Image from "next/image";
+import SelectCampus from "../ui/campus/selectCampus";
+import { useSearchParams } from "next/navigation";
 
-export default function Blog(campus: any) {
+export default function Blog({
+    campus,
+    searchParams,
+}: {
+    searchParams?: { campus?: string };
+    campus: string;
+}) {
     const blogDir = "campus";
 
     const files = fs.readdirSync(path.join(blogDir));
@@ -21,36 +29,74 @@ export default function Blog(campus: any) {
             slug: filename.replace(".mdx", ""),
         };
     });
+
+    const search = searchParams?.campus || "";
+    console.log({ search: search });
+
     return (
         <div>
-            <div className="min-h-screen justify-center flex flex-col">
+            <div className={`min-h-screen justify-center flex flex-col`}>
                 <h2 className="text-4xl font-bold mt-32 mb-4">Blog</h2>
-                <select className="border-2 border-slate-300 rounded-lg p-2 mb-10 bg-pewter">
-                    {blogs.map((blog) => (
-                        <option key={blog.slug}>{blog.meta.title}</option>
-                    ))}
-                </select>
-                <div className="grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 gap-5 grid-cols-1 m-2 mb-10">
-                    {blogs.map((blog) => (
-                        <Link
-                            href={`/campus/${blog.slug}`}
-                            passHref
-                            key={blog.slug}
-                            className="shadow-lg bg-sage hover:shadow-2xl space-y-4 items-center flex flex-col transition duration-300 ease-in-out rounded-lg p-2 w-full"
+                <SelectCampus name={blogs.map((blog) => blog.meta.title)} />
+                {search ? (
+                    <div>
+                        <div
+                            className={`grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 gap-5 grid-cols-1 m-2 mb-10 `}
                         >
-                            <Image
-                                src={blog.meta.image}
-                                alt={blog.meta.title}
-                                width={200}
-                                height={300}
-                                className="justify-center h-60 items-center m-auto  rounded-lg w-60"
-                            />
-                            <p className="mt-3 text-lg font-semibold">
-                                {blog.meta.title}
-                            </p>
-                        </Link>
-                    ))}
-                </div>
+                            {blogs.map((blog) => (
+                                <Link
+                                    href={`/campus/${blog.slug}`}
+                                    passHref
+                                    key={blog.meta.title}
+                                    className={`shadow-lg bg-sage hover:shadow-2xl space-y-4 items-center flex flex-col transition duration-300 ease-in-out rounded-lg p-2 w-full
+                                    ${
+                                        search === blog.meta.title
+                                            ? ""
+                                            : "hidden"
+                                    }
+                                    `}
+                                >
+                                    <Image
+                                        src={blog.meta.image}
+                                        alt={blog.meta.title}
+                                        width={200}
+                                        height={300}
+                                        className="justify-center h-60 items-center m-auto  rounded-lg w-60"
+                                    />
+                                    <p className="mt-3 text-lg font-semibold">
+                                        {blog.meta.title}
+                                    </p>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <div
+                            className={`grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 gap-5 grid-cols-1 m-2 mb-10 `}
+                        >
+                            {blogs.map((blog) => (
+                                <Link
+                                    href={`/campus/${blog.slug}`}
+                                    passHref
+                                    key={blog.meta.title}
+                                    className={`shadow-lg bg-sage hover:shadow-2xl space-y-4 items-center flex flex-col transition duration-300 ease-in-out rounded-lg p-2 w-full`}
+                                >
+                                    <Image
+                                        src={blog.meta.image}
+                                        alt={blog.meta.title}
+                                        width={200}
+                                        height={300}
+                                        className="justify-center h-60 items-center m-auto  rounded-lg w-60"
+                                    />
+                                    <p className="mt-3 text-lg font-semibold">
+                                        {blog.meta.title}
+                                    </p>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
