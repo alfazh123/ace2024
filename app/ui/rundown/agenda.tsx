292ec20196cb3805c55fb27e238a9a4f147daa46
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-23 11:57:20 +0700

    feat faq component

--- app/ui/rundown/agenda.tsx
+++ app/ui/rundown/agenda.tsx
@@ -1,26 +1,31 @@
 interface Schedule {
-  name: string;
-  data: {
-    time: string;
-    agenda: string;
-  }[];
+    name: string;
+    data: {
+        time: string;
+        agenda: string;
+    }[];
 }
 
 export default function Agenda({ data, name }: Schedule) {
-  return (
-    <div>
-      <h3 className="text-2xl font-semibold">{name}</h3>
-      <ul className="list-disc space-y-4 my-5 ml-4">
-        {data.map((item, index) => (
-          <li key={index}>
-            <div className="timeline-marker"></div>
-            <div className="">
-              <p className="text-md">{item.time}</p>
-              <p className="text-lg font-semibold">{item.agenda}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="mb-10 md:max-w-96">
+            <h3 className="text-2xl font-semibold ml-4">{name}</h3>
+            <ul className="list-disc ml-4">
+                {data.map((item, index) => (
+                    <li
+                        key={index}
+                        className="border-solid border-t-2 border-oliver last:border-b-2 h-18"
+                    >
+                        <div className="timeline-marker"></div>
+                        <div className="">
+                            <p className="text-lg font-semibold">
+                                {item.agenda}
+                            </p>
+                            <p className="text-md italic">{item.time}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
