ae48a266c8e00f4568e7f09583776f6a191e5c2e
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-23 20:32:27 +0700

    feat: select option for campus

---
+++ app/ui/campus/selectCampus.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+
+interface selectCampusProps {
+    name: string[];
+}
+
+export default function SelectCampus({ name }: selectCampusProps) {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    function handleSearch(term: string) {
+        const params = new URLSearchParams(searchParams);
+        if (term) {
+            params.set("campus", term);
+        } else {
+            params.delete("campus");
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    return (
+        <div>
+            <select
+                className={`border-2 border-slate-300 rounded-lg p-2 mb-10 bg-pewter`}
+                title="campus"
+                value=""
+                onChange={(e) => handleSearch(e.target.value)}
+                defaultValue={searchParams.get("campus")?.toString()}
+            >
+                {name.map((campus) => (
+                    <option key={campus} value={campus}>
+                        {campus}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
