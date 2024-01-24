c5cd34321d9b0a881633b72a6d6591d9930c32c2
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-24 15:25:23 +0700

    fix: responsive

--- app/campus/page.tsx
+++ app/campus/page.tsx
@@ -37,32 +37,36 @@ export default async function Blog() {
     return (
         <div>
             <div className={`min-h-screen justify-center flex flex-col `}>
-                <h2 className="text-4xl font-bold mt-32 mb-4">Blog</h2>
+                <h2 className="text-4xl font-bold mt-32 mb-4">Campus</h2>
 
-                <SelectCampus name={blogs.map((blog) => blog.meta.title)} />
+                {/* <SelectCampus name={blogs.map((blog) => blog.meta.title)} /> */}
 
                 <div
-                    className={`grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 gap-5 grid-cols-1 m-2 mb-10 `}
+                    className={`grid lg:grid-cols-3 md:grid-cols-2 md:gap-4 gap-5 grid-cols-1 mb-10 `}
                 >
                     {blogs.map((blog) => (
                         <Link
                             href={`/campus/${blog.slug}`}
                             passHref
                             key={blog.meta.title}
-                            className={`shadow-lg bg-sage hover:shadow-2xl space-y-4 items-center flex flex-col transition duration-300 ease-in-out rounded-lg p-2 w-full
-                            
-                            `}
+                            className={`shadow-lg bg-pewter border-solid border-2 border-oliver hover:shadow-2xl space-y-4 md:items-center flex md:flex-col md:space-x-0 space-x-4 flex-row transition duration-300 ease-in-out rounded-lg p-2 w-full`}
                         >
                             <Image
                                 src={blog.meta.image}
                                 alt={blog.meta.title}
                                 width={200}
                                 height={300}
-                                className="justify-center h-60 items-center m-auto  rounded-lg w-60"
+                                className="justify-center md:h-60 md:w-60 sm:w-32 sm:h-32 h-20 w-20 md:items-center m-auto  rounded-lg"
                             />
-                            <p className="mt-3 text-lg font-semibold">
+                            <p className="text-lg md:flex hidden items-center justify-center text-center font-bold w-full">
                                 {blog.meta.title}
                             </p>
+                            <div className="md:hidden flex flex-col w-5/6">
+                                <p className="mt-3 sm:text-lg text-sm md:font-semibold font-bold md:w-full w-5/6">
+                                    {blog.meta.title}
+                                </p>
+                                <p>{blog.meta.location}</p>
+                            </div>
                         </Link>
                     ))}
                 </div>
