41bb9269030a85a0ef8e0400d3f0f985476fb35f
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-01-24 16:03:32 +0700

    feat: page link

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,37 +1,67 @@
 import Image from "next/image";
-import { FaInstagram } from "react-icons/fa";
+import Link from "next/link";
+import {
+    FaInstagram,
+    FaHome,
+    FaQuestion,
+    FaSchool,
+    FaUserFriends,
+} from "react-icons/fa";
 
 export default function Footer() {
-  return (
-    <footer className="bg-sagegreen text-olive pt-10">
-      <div className="container md:px-20 px-10 py-4 flex flex-col md:flex-row md:justify-between md:items-center space-y-5 md:space-x-5">
-        <figure className="w-32">
-          <Image
-            src="/ace2024.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </figure>
-        <div>
-          <p className="text-xl font-bold">Sosial</p>
-          <a href="https://www.instagram.com/ace_.2k24/" className="flex">
-            <FaInstagram className={`mr-2 text-xl`} />
-            ACE2024
-          </a>
-        </div>
-        <div>
-          <p className="text-xl font-bold">Alamat</p>
-          <p>
-            Jl. Raya Bungah Nomor 1 Sampurnan Bungah 61152, Gresik, Jawa Timur
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-between mx-auto px-20 py-10">
-        <p className="text-sm">© 2023 ACE</p>
-        <p className="text-sm">All rights reserved</p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="bg-sagegreen text-olive pt-10">
+            <div className="container md:px-20 px-10 py-4 flex flex-col md:flex-row md:justify-between space-y-5 md:space-x-5">
+                <figure className="w-32">
+                    <Image
+                        src="/ace2024.svg"
+                        alt="Next.js Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+                </figure>
+                <div>
+                    <p className="text-xl font-bold">Sosial</p>
+                    <a
+                        href="https://www.instagram.com/ace_.2k24/"
+                        className="flex"
+                    >
+                        <FaInstagram className={`mr-2 text-xl`} />
+                        ACE2024
+                    </a>
+                </div>
+                <div>
+                    <p className="text-xl font-bold">Halaman</p>
+                    <Link href="/" className="flex">
+                        <FaHome className={`mr-2 text-lg`} />
+                        Home
+                    </Link>
+                    <Link href="/about" className="flex">
+                        <FaQuestion className={`mr-2 text-lg`} />
+                        About
+                    </Link>
+                    <Link href="/campus" className="flex">
+                        <FaSchool className={`mr-2 text-lg`} />
+                        Campus
+                    </Link>
+                    <Link href="/partner" className="flex">
+                        <FaUserFriends className={`mr-2 text-lg`} />
+                        Partner
+                    </Link>
+                </div>
+                <div>
+                    <p className="text-xl font-bold">Alamat</p>
+                    <p>
+                        Jl. Raya Bungah Nomor 1 Sampurnan Bungah 61152, Gresik,
+                        Jawa Timur
+                    </p>
+                </div>
+            </div>
+            <div className="flex justify-between mx-auto px-20 py-10">
+                <p className="text-sm">© 2023 ACE</p>
+                <p className="text-sm">All rights reserved</p>
+            </div>
+        </footer>
+    );
 }
